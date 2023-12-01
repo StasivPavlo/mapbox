3536d6b54e2c980ed20b0b9a2a964ab84203fea2
pavlo <pavlostasiv@gmail.com>
2023-12-01 14:30:42 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from 'react';
+import Map, { MapLayerMouseEvent, Marker } from 'react-map-gl';
+import { getFirestore, doc, updateDoc } from "firebase/firestore";
+import { app } from './firebase';
+
+interface Location {
+  Lat: number;
+  Long: number;
+}
+
+interface Quest {
+  [key: string]: {
+    Location: Location;
+    Timestamp: string;
+    Next?: Quest | null;
+  };
+}
+
+interface Marker {
+  Location: Location
+  Timestamp: Date,
+  next?: Marker,
+}
+
+const db = getFirestore(app);
+const currentDocRef = doc(db, 'map', 'markers')
 
 function App() {
+  const [markers, setMarkers] = useState<Marker[]>([{
+    Location: { Lat: 51.3233379650232, Long: -0.481747846041145 },
+    Timestamp: new Date()
+  }]);
+  const [markerObj, setMarkerObj] = useState<Quest | null>(null)
+
+  const onClick = useCallback((e: MapLayerMouseEvent) => {
+    const { lat, lng } = e.lngLat;
+
+    const isIncludeMarker = Boolean(markers.find(({ Location }) => Location.Lat + Location.Long === lat + lng));
+
+    if (!isIncludeMarker) {
+      setMarkers(state => [...state, { Location: { Lat: lat, Long: lng }, Timestamp: new Date() }]);
+    }
+  }, [markers]);
+
+  useEffect(() => {
+    setMarkerObj(convertToQuests(markers));
+  }, [markers]);
+
+  useEffect(() => {
+    if (currentDocRef) {
+      updateDoc(currentDocRef, {
+        ...markerObj,
+      })
+    }
+  }, [markerObj]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Map
+      mapboxAccessToken="pk.eyJ1IjoidjFtZXIyMTA5IiwiYSI6ImNscGxmdGdzYjA1MXYyam1yeDdqZmRnZzAifQ.rqHAj27Or6zU03KJKL_lUA"
+      style={{ width: '100wh', height: '100vh' }}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+      onClick={onClick}
+    >
+      {markers.map((marker, i) => (
+        <Marker
+          longitude={marker.Location.Long}
+          latitude={marker.Location.Lat}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <img src={`https://marker.nanoka.fr/map_pin-CC0000-FFF-333-${i + 1}-40.svg`} alt="" />
+        </Marker>
+      ))}
+    </Map>
   );
 }
 
 export default App;
+
+const convertToQuests = (markers: Marker[], questNumber = 1): Quest | null => {
+  if (markers.length === 0) {
+    return null;
+  }
+
+  const quest: Quest = {
+    [`Quest ${questNumber}`]: {
+      Location: {
+        Lat: markers[0].Location.Lat,
+        Long: markers[0].Location.Long,
+      },
+      Timestamp: markers[0].Timestamp.toLocaleString(),
+      Next: convertToQuests(markers.slice(1), questNumber + 1),
+    },
+  };
+
+  return quest;
+};
